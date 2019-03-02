51a8abc41b4f1c4819001b4f7fd6d5023e74bd62
bekk blando <bekkblando@bekks-MacBook-Pro.local>
2019-03-01 23:55:01 -0500

    latest UI Stuff

--- src/App.js
+++ src/App.js
@@ -12,7 +12,10 @@ class App extends Component {
         lat: 0,
         lng: 0
       }],
-      vocabulary: '',
+      vocabulary: [{
+        word: "",
+        meaning: ""
+      }],
       startdate: '',
       enddate: '',
       notes: ''
@@ -50,51 +53,94 @@ class App extends Component {
     this.setState({locations: locations})
   }
 
+  setWord(event, idx){
+    let words = this.state.vocabulary
+    words[idx]["word"] = event.target.value
+    console.log(words)
+    this.setState({vocabulary: words})
+  }
+
+  setMeaning = (event, idx) => {
+    let words = this.state.vocabulary
+    words[idx]["meaning"] = event.target.value
+    console.log(words)
+    this.setState({vocabulary: words});
+  }
+
+  addVocabulary = () => {
+    let words = this.state.vocabulary;
+    words.push({word: "", meaning: ""});
+    this.setState({vocabulary: words})
+  }
+
   render() {
     let locations = this.state.locations;
-    console.log(locations)
+    let vocabulary = this.state.vocabulary;
     return (
-      <form onSubmit={this.buildJSON}>
-        <label>
-          Name:
-          <input type="text" onChange = {(event) => this.setState({name: event.target.value})}/>
-        </label>
-        <button onClick={ this.addLocation }>Add Location</button>
-        {
-          locations.map((val, idx) => {
+      <div class = "container">
+        <form onSubmit={this.buildJSON}>
+
+      <div class = "row">
+        <div class = "col s6">
+            <h1>Name:</h1>
+            <input class="input-field" type="text" onChange = {(event) => this.setState({name: event.target.value})}/>
+            <h1>Location</h1>
+            <button onClick={ this.addLocation }>Add Location</button>
+          {
+            locations.map((val, idx) => {
+                return(
+                  <div>
+                    <label>
+                      Longitude:
+                      <input type="number" onChange = {(event) => this.setLocationLng(event, idx)}/>
+                      </label>
+                      <label>
+                      Latitude:
+                      <input type="number" onChange = {(event) => this.setLocationLat(event, idx)}/>
+                    </label>
+                  </div>
+                );
+            })
+          }
+        </div>
+      </div>
+
+        <div class="col s6">
+          <h1>Vocabulary:</h1>
+          {
+            vocabulary.map((val, idx) => {
               return(
                 <div>
                   <label>
-                    Longitude:
-                    <input type="number" onChange = {(event) => this.setLocationLng(event, idx)}/>
+                    Word
+                    <input class="input-field" type="text" onChange = {(event) => this.setWord(event, idx)}/>
                   </label>
                   <label>
-                    Latitude:
-                    <input type="number" onChange = {(event) => this.setLocationLat(event, idx)}/>
+                    Meaning
+                    <input class="input-field" type="text" onChange = {(event) => this.setMeaning(event, idx)}/>
                   </label>
                 </div>
               );
-          })
-        }
-          
-        <label>
-          Vocabulary:
-          <input type="text" onChange = {(event) => this.setState({vocabulary: event.target.value })}/>
-        </label>
-        <label>
-          Start Date:
-          <input type="datetime" onChange = {(event) => this.setState({startdate: event.target.value })}/>
-        </label>
-        <label>
-          End Date:
-          <input type="endtime" onChange = {(event) => this.setState({enddate: event.target.value })}/>
-        </label>
-        <label>
-          Notes:
-          <input type="text" onChange = {(event) => this.setState({notes: event.target.value })}/>
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+            })
+          }
+          <button onClick={ this.addVocabulary }>Add Vocabulary</button>
+      </div>
+
+          <label>
+            <h1>Start Date:</h1>
+            <input type="datetime" class="input-field" onChange = {(event) => this.setState({startdate: event.target.value })}/>
+          </label>
+          <label>
+            <h1>End Date:</h1>
+            <input type="endtime" class="input-field"  onChange = {(event) => this.setState({enddate: event.target.value })}/>
+          </label>
+          <label>
+            <h1>Notes:</h1>
+            <input class="input-field" type="text" onChange = {(event) => this.setState({notes: event.target.value })}/>
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
 }
